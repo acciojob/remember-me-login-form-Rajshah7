a1fd3b0a2b6f730db18d6727d5d4c9f0ca57d70f
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-29 00:59:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let checkbox = document.getElementById('checkbox');
+let form = document.getElementById('loginForm');
+let existingButton = document.getElementById('existing');
+
+window.onload = function(){
+	const username = localStorage.getItem("username");
+	const password = localStorage.getItem("password");
+
+	if(username && password){
+			existingButton.style.display = 'block';
+	}
+}
+
+form.addEventListener('submit', (e) =>{
+	e.preventDefault();
+
+	const user = username.value;
+	const pass = password.value;
+
+	alert(`Logged in as ${user}`);
+
+	if(checkbox.checked){
+		localStorage.setItem('username',user);
+		localStorage.setItem('password',pass);
+	}else{
+		localStorage.removeItem('username');
+		localStorage.removeItem('password');
+	}
+});
+
+existingButton.addEventListener('click',()=>{
+	const savedUsername = localStorage.getItem('username');
+
+	alert(`Logged in as ${savedUsername}`);
+});
+
+
